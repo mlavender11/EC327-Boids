30ff7ee904456e382ec93b7b0ed851f0334df579
jiyongka <kyle@mountainrunner.us>
2026-04-21 15:02:00 -0400

    Added bash script to run easier

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "🧹 Clearing old CMake cache..."
+rm -rf build
+mkdir build
+cd build
+
+echo "⚙️ Configuring CMake..."
+cmake ..
+
+echo "🔨 Building project..."
+cmake --build .
+
+cd ..
+
+echo "🚀 Running Boids..."
+./build/EC327-Boids
