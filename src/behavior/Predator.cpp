68a159b6c7a94b5d8a0221da1f488b7235be0c1b
Michael Lavender <michaelzlavender@gmail.com>
2026-04-21 14:16:07 -0400

    run test

--- src/behavior/Predator.cpp
+++ src/behavior/Predator.cpp
@@ -1,28 +1,28 @@
-#include "Predator.h"
+// #include "Predator.h"
 
-Predator::Predator(): Boids() 
-{
-    target_flock = nullptr;
-    target = nullptr;
-}
+// Predator::Predator(): Boids() 
+// {
+//     target_flock = nullptr;
+//     target = nullptr;
+// }
 
-Predator::Predator(Flock& new_target_flock): Boids() 
-{
-    target_flock = &new_target_flock;
-    target = nullptr;
-}
+// Predator::Predator(Flock& new_target_flock): Boids() 
+// {
+//     target_flock = &new_target_flock;
+//     target = nullptr;
+// }
 
-void Predator::get_target(){
-    if(target_flock == nullptr || target_flock->GetSizeOfFLock() == 0) return;
+// void Predator::get_target(){
+//     if(target_flock == nullptr || target_flock->GetSizeOfFlock() == 0) return;
 
-    target = &target_flock->Get_Friendly(0);
+//     target = &target_flock->Get_Friendly(0);
 
-    if(target_flock->GetSizeOfFLock() == 1)return;
+//     if(target_flock->GetSizeOfFlock() == 1)return;
 
 
-    for(int i = 1; i < target_flock->GetSizeOfFLock(); i++){
-        if (getDistance(*this, target_flock->Get_Friendly(i)) < getDistance(*this, *target)){
-            target = &target_flock->Get_Friendly(i);
-            }
-    }
-}
+//     for(int i = 1; i < target_flock->GetSizeOfFlock(); i++){
+//         if (getDistance(*this, target_flock->Get_Friendly(i)) < getDistance(*this, *target)){
+//             target = &target_flock->Get_Friendly(i);
+//             }
+//     }
+// }
--- src/behavior/Predator.h
+++ src/behavior/Predator.h
@@ -1,19 +1,19 @@
-#ifndef PREDATOR_H
-#define PREDATOR_H
-#include "Boids.h" // CMakeFiles.txt is set up to find this header file - Kyle
-#include "Friendly.h"
-#include "Flock.h"
+// #ifndef PREDATOR_H
+// #define PREDATOR_H
+// #include "Boids.h" // CMakeFiles.txt is set up to find this header file - Kyle
+// #include "Friendly.h"
+// #include "Flock.h"
 
-class Predator : public Boids
-{
-public:
-    Friendly *target;
-    Flock *target_flock;
+// class Predator : public Boids
+// {
+// public:
+//     Friendly *target;
+//     Flock *target_flock;
 
-    Predator();
-    Predator(Flock &new_target_flock);
-    void get_target();
-    void update();
-};
+//     Predator();
+//     Predator(Flock &new_target_flock);
+//     void get_target();
+//     void update();
+// };
 
-#endif
+// #endif
