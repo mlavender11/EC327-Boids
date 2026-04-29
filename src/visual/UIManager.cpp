fc781b55a0b0469ee8022073b8b0a8df7c4003c6
jiyongka <kyle@mountainrunner.us>
2026-04-28 20:37:50 -0400

    Added description in setup menu

--- src/visual/UIManager.cpp
+++ src/visual/UIManager.cpp
@@ -76,8 +76,11 @@ bool UIManager::RenderSetupMenu(int &boidCount, float &earthRadius, float &minAl
     bool startClicked = false;
     ImGuiIO &io = ImGui::GetIO();
 
-    ImGui::SetNextWindowPos(ImVec2(io.DisplaySize.x * 0.5f, io.DisplaySize.y * 0.5f), ImGuiCond_Always, ImVec2(0.5f, 0.5f));
-    ImGui::SetNextWindowSizeConstraints(ImVec2(400, -1), ImVec2(io.DisplaySize.x, io.DisplaySize.y));
+    // Anchor the RIGHT edge (1.0f, 0.5f) of this window 10 pixels to the left of the screen center
+    ImGui::SetNextWindowPos(ImVec2(io.DisplaySize.x * 0.5f - 10.0f, io.DisplaySize.y * 0.5f), ImGuiCond_Always, ImVec2(1.0f, 0.5f));
+
+    // Allow it to auto-resize, but prevent it from taking up more than half the screen
+    ImGui::SetNextWindowSizeConstraints(ImVec2(400, -1), ImVec2(io.DisplaySize.x * 0.5f, io.DisplaySize.y));
 
     ImGuiWindowFlags flags = ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse;
     ImGui::Begin("Simulation Setup", nullptr, flags);
@@ -133,6 +136,47 @@ bool UIManager::RenderSetupMenu(int &boidCount, float &earthRadius, float &minAl
         quit = true;
 
     ImGui::End();
+
+    // --- DESCRIPTION WINDOW ---
+
+    // Anchor the LEFT edge (0.0f, 0.5f) of this window 10 pixels to the right of the screen center
+    ImGui::SetNextWindowPos(ImVec2(io.DisplaySize.x * 0.5f + 10.0f, io.DisplaySize.y * 0.5f), ImGuiCond_Always, ImVec2(0.0f, 0.5f));
+
+    // Force the max width to be slightly less than half the screen so it wraps the text
+    // cleanly instead of bleeding off the right edge of the monitor.
+    ImGui::SetNextWindowSizeConstraints(ImVec2(350, -1), ImVec2(io.DisplaySize.x * 0.45f, io.DisplaySize.y));
+
+    ImGuiWindowFlags descFlags = ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse;
+    ImGui::Begin("About This Simulation", nullptr, descFlags);
+
+    ImGui::Spacing();
+    ImGui::Separator();
+    ImGui::Spacing();
+
+    ImGui::TextWrapped("Our program simulates the flocking behavior of animals using Craig Reynolds' 1986 Boids algorithm. Each boid follows (at least) these three basic rules:");
+    ImGui::Spacing();
+
+    // BulletText automatically indents and adds a bullet point
+    ImGui::BulletText("Separation: Avoid crowding with neighbors.");
+    ImGui::BulletText("Alignment: Steer towards the average heading of its neighbors.");
+    ImGui::BulletText("Cohesion: Steer towards the average position of its neighbors.");
+
+    ImGui::Spacing();
+    ImGui::Separator();
+    ImGui::Spacing();
+
+    ImGui::TextWrapped("We added predators, which will occasionally hunt the flock. When hungry, they'll turn red and target the closest boid. Once they eat, they'll become docile until they get hungry again. Normal Boids try to run away from predators.");
+
+    ImGui::Spacing();
+
+    ImGui::TextWrapped("Use the sliders to the left to set up your initial conditions, then click 'Start Simulation' to begin.");
+
+    ImGui::Spacing();
+
+    ImGui::TextWrapped("Once in the simulation, you can adjust behaviors and physics using the menu on the left, and move around by clicking and dragging, and by scrolling. There's also a pause and options menu accessible via button or by pressing escape.");
+
+    ImGui::End();
+
     return startClicked;
 }
 
@@ -292,7 +336,7 @@ void UIManager::RenderGraphicsMenu(UITheme &currentTheme, bool &backClicked)
         currentTheme = UITheme::SYNTHWAVE;
         ApplyTheme(UITheme::SYNTHWAVE);
     }
-    if (ImGui::RadioButton("WarGames", currentTheme == UITheme::COLDWAR))
+    if (ImGui::RadioButton("WarGames full graphics theme", currentTheme == UITheme::COLDWAR))
     {
         currentTheme = UITheme::COLDWAR;
         ApplyTheme(UITheme::COLDWAR);
